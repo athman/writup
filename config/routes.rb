8866618a849f3610a34a5e762f02bd3d0ae8d07e
Athman Gude <athmangude@gmail.com>
2014-01-09 12:30:07 +0300

    Added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Writup::Application.routes.draw do
+  get "pages/home"
+  get "pages/help"
+  get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
