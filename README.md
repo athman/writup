a76ca815a772111fee499411fb8f728b7b652d5e
Athman Gude <athmangude@gmail.com>
2014-01-09 10:28:51 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a public blogging application built with Rails. It is purely for my personal education on the Ruby on Rails framework
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Writup::Application.config.secret_key_base = '1968ef73aff76e2094d28ad943b54cb20813cba09bb212be0aa71244cc24ff79e7bee8c11eeeed5407a45597f97a90ee027de5a82d8e8abbc18dbab53b03b537'
