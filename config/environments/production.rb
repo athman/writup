039ecbc10808796154a11341e9c366c3d7e5f629
Athman Gude <athmangude@gmail.com>
2014-01-12 00:03:44 +0300

    Enabled access to the app via SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Writup::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -77,4 +77,8 @@ Writup::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
