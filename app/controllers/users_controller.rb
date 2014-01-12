590832c294a5618f268a6a9bfd8e59254a173b74
Athman Gude <athmangude@gmail.com>
2014-01-12 14:43:55 +0300

    Changed the welcome message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Hi #{@user.first_name}, welcome to Writup. We are pleased to have you here (:<)"
+      flash[:success] = "Hi #{@user.first_name}, welcome to Writup. We are pleased to have you here (:<). Please check back every often to see new features to the site. We want you to be among the veterans on Writup"
       redirect_to @user
     else
       render 'new'
