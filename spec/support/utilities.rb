1902e14a0d49c6910a62ae1388b7b379db7b7a99
Athman Gude <athmangude@gmail.com>
2014-01-20 14:33:07 +0300

    Updated code with tutorial code

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 # A helper for page titles
 def full_title page_title
   base_title = "Writup"
@@ -5,5 +7,17 @@ def full_title page_title
     base_title
   else
     "#{base_title}::#{page_title}"
+  end
+end
+
+def valid_signin user
+  fill_in "session_email",        with: user.email
+  fill_in "session_password",     with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector("div.alert.alert_danger")
   end
 end
